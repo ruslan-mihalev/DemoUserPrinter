b923e307afd10f4c46ccfc08c396feaafe9b14b7
Ruslan Mikhalev <ruslan.mikhalev@2degrees.nz>
2023-09-03 17:24:30 +1200

    Initial Commit

---
+++ DemoUserPrinter/DemoUserPrinter.h
@@ -0,0 +1,18 @@
+//
+//  DemoUserPrinter.h
+//  DemoUserPrinter
+//
+//  Created by Ruslan.Mikhalev on 3/09/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DemoUserPrinter.
+FOUNDATION_EXPORT double DemoUserPrinterVersionNumber;
+
+//! Project version string for DemoUserPrinter.
+FOUNDATION_EXPORT const unsigned char DemoUserPrinterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DemoUserPrinter/PublicHeader.h>
+
+
